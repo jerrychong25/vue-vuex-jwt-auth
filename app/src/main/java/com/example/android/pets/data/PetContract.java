65b79d21ccd8b076c72ba2884aa156c87f9f62be
Jerry Chong <15714095+jerrychong25@users.noreply.github.com>
2018-11-17 16:07:29 +0800

    Added PetContract.java

---
+++ app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -0,0 +1,26 @@
+package com.example.android.pets.data;
+
+import android.provider.BaseColumns;
+
+public class PetContract {
+
+    private PetContract() {}
+
+    public static final class PetEntry implements BaseColumns {
+
+        public static final String TABLE_NAME = "pets";
+
+        public static final String _ID = BaseColumns._ID;
+        public static final String COLUMN_NAME = "name";
+        public static final String COLUMN_BREED = "breed";
+        public static final String COLUMN_GENDER = "gender";
+        public static final String COLUMN_WEIGHT = "weight";
+
+        /**
+         * Possible values for the genders of the pets.
+         */
+        public static final int GENDER_UNKNOWN = 0;
+        public static final int GENDER_MALE = 1;
+        public static final int GENDER_FEMALE = 2;
+    }
+}
